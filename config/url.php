ec4681cc11a35e3d8b9df4f9a613e75df0efa588
withvisionstack <stafproject125pm@gmail.com>
2026-02-23 18:08:56 -0300

    Adiciona dotenv para proteger chave da API

--- config/url.php
+++ config/url.php
@@ -1,10 +1,16 @@
 <?php
 
-// BASE_URL do site (CSS, JS, imagens, links internos)
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// BASE_URL do site
 $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . "?") . "/";
 
 // API do Unsplash
 define('UNSPLASH_BASE_URL', 'https://api.unsplash.com/');
-define('UNSPLASH_ACCESS_KEY', 'DI4hgskAFGspnVvvRwSO0SwpAFBdq8HbWfoQDpJIzs8');
-// Caminho para cache local (opcional)
+define('UNSPLASH_ACCESS_KEY', $_ENV['UNSPLASH_ACCESS_KEY']);
+
+// Cache
 define('CACHE_PATH', __DIR__ . '/../images/cache.json');
